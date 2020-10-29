db994a7027c068cfc249c0a3587775284e14cc54
Armen Matevosyan <aamatevosyan@edu.hse.ru>
2020-10-29 10:59:31 +0400

    Redesigned components

--- src/components/BtnWidget/index.js
+++ src/components/BtnWidget/index.js
@@ -6,7 +6,7 @@ const BtnWidget = (props) => {
         <div className={"btn-widget"}>
             <h2 className="step-title">Step: {props.score} </h2>
             <Btn color={props.color} className={props.className} onClick={props.onClick}/>
-            <p>You can click to the square above to undo the last current color selection.</p>
+            <p>You can click to the square above to undo the last color selection.</p>
         </div>
     )
 };
